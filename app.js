a53ff949e6c4a7f4a7e12dcaf74723deb17ea224
gretchenhu <huiqinhu88@gmail.com>
2023-11-10 17:16:14 -0800

    updated app.js

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 // app.js
 import express, { json, urlencoded } from "express";
-import { join } from "path";
+import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 //import indexRouter from "./routes/index";
 //import usersRouter from "./routes/users";
-import imagesRouter from "./routes/imagesRouter"; 
+import imagesRouter from "./routes/imagesRouter";
 import commentsRouter from "./routes/commentsRouter"; // import the images router
 
-const app = express();
+let app = express();
 
 app.use(logger("dev"));
-app.use(json());
-app.use(urlencoded({ extended: false }));
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 //app.use("/", indexRouter);
 //app.use("/users", usersRouter);
