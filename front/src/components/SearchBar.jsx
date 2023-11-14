6945f93988a50a963c1f35c6c43ce2abf8fc0d35
gretchenhu <huiqinhu88@gmail.com>
2023-11-13 21:55:32 -0800

    added search bar

---
+++ front/src/components/SearchBar.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+
+export default function SearchBar({ query, setQuery }) {
+  function onInput(evt) {
+    setQuery(evt.target.value);
+  }
+
+  return (
+    <div>
+      <label className="form-label">
+        Search by Dynasty (case sensitive):{" "}
+        <input
+          id="search"
+          className="form-control"
+          type="search"
+          value={query}
+          onInput={onInput}
+        ></input>
+      </label>
+    </div>
+  );
+}
+SearchBar.propTypes = {
+  query: PropTypes.string.isRequired,
+  setQuery: PropTypes.func.isRequired,
+};
--- front/src/pages/Gallery.jsx
+++ front/src/pages/Gallery.jsx
@@ -2,48 +2,43 @@ import { useState, useEffect } from "react";
 import Navbar from "../components/navbar/Navbar";
 import PhotosGallery from "../components/PhotosGallery";
 import Footer from "../components/footer/Footer";
+import SearchBar from "../components/SearchBar";
 
 function GalleryPage() {
-  let [photos, setPhotos] = useState([]);
+  const [query, setQuery] = useState("");
+  const [photos, setPhotos] = useState([]);
 
-  async function testBack() {
-    console.log("Testing back...");
+  async function fetchPhotos() {
+    console.log("Fetching photos...");
     const response = await fetch("api/buddha");
-    const data = await response.json();
-    console.log("Got Data!", data);
+    if (!response.ok) {
+      console.log("Error fetching photos", response);
+      return;
+    }
+    const __photos = await response.json();
+    console.log("Got Data!", __photos);
 
-    setPhotos(data);
-    console.log("Render App photos=", data);
+    setPhotos(__photos);
+    console.log("Render App photos=", __photos);
   }
 
   useEffect(() => {
     console.log("App useEffect called");
-    testBack();
+    fetchPhotos();
 
-    return () => {
-      //cleanup for the effect
-    };
+    return () => {};
   }, []);
 
   return (
     <div>
       <Navbar />
       <h1>Buddha World Gallery</h1>
-      <PhotosGallery photos={photos} />
+
+      <SearchBar query={query} setQuery={setQuery} />
+      <PhotosGallery photos={photos.filter((d) => d.dynasty.includes(query))} />
       <Footer />
     </div>
   );
 }
 
 export default GalleryPage;
-
-// const GalleryPage = () => {
-//   return (
-//     <div>
-//       <Navbar />
-//       <PhotosGallery />
-//     </div>
-//   );
-// };
-
-// export default GalleryPage;
